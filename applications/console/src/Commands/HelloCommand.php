e7ff4a6ea0d30b26007672f65a744bd10b01b8e1
Justin Chen <cxd032404@hotmail.com>
2020-04-08 12:13:25 +0800

    merge

--- applications/console/src/Commands/HelloCommand.php
+++ applications/console/src/Commands/HelloCommand.php
@@ -19,6 +19,12 @@ class HelloCommand
     public function main()
     {
         //php mix-console hl -n=111 --say=222
+        /*
+         *          $name = Flag::string(['n', 'name'], 'Xiao Ming');
+        $say  = Flag::string('say', 'Hello, World!');
+        println("{$name}: {$say}");
+
+         */
         $redis = app()->redis;
         $redis_key = "redis_test";
         $rand = rand(1,9);
